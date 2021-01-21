67498c28292c64b1dae72e823f0e2a1c1d7bbc6f
sanvit <sanvit@sanvit.me>
2021-01-21 05:33:13 +0000

    fix mentions

--- mainApps/mentions.py
+++ mainApps/mentions.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import CatMention, KitchenMention, EmergencyMention, Cat, Kitchen, Mention, User
 from django.contrib.auth.decorators import login_required
 from .sms import *
+from django.http import HttpResponse
+
 
 @login_required
 def newCatMention(req):
@@ -29,15 +31,15 @@ def newEmergencyMention(req):
         m = EmergencyMention()
         kitchen = Kitchen.objects.get(pk=req.POST['kid'])
         m.target = kitchen
-        m.save()
-        m = EmergencyMention.objects.get(m)
-        m.mention.set(mention)
+        m.mention = mention
         m.save()
         sms_user = User.objects.filter(favoriteKitchen=kitchen)
         to = []
         for a in sms_user:
             to.append(a.phoneNumber)
-        print(to)
+        text = f"{kitchen.name}에서 아래 내용의 긴급 메시지가 등록되었습니다.\n{mention.mention}"
+        sms(to,text)
+        return HttpResponse(str(to))
     else:
         return render(req,'emergency.html')
 
