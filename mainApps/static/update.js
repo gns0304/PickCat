995a314013f1368270378c5b3d7998d77310f13e
sanvit <sanvit@sanvit.me>
2021-01-21 08:40:17 +0000

    add update JS

---
+++ mainApps/static/update.js
@@ -0,0 +1,26 @@
+window.latest = 0;
+let chatbox = document.getElementById('chatbox')
+const ENDPOINT = 'getCatMentions'
+function update() {
+    let url = `/${ENDPOINT}?id=1&latest=${window.latest}`
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = 'json';
+    xhr.send();
+    xhr.onreadystatechange = (e) => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            data = xhr.response
+            if (data.count != 0) {
+                window.latest = data.data[0].id
+                writeData(data.data)
+            }
+        }
+    }
+}
+function writeData(data) {
+    for (k in data) {
+        chatbox.innerHTML += `<span>${data[k].text}</span><br>\n`
+    }
+}
+function update()
+window.setInterval(update, 3000)
